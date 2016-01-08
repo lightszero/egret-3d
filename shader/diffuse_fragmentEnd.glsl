6c6d11f716cbcb99469d1127e884ba98a94af026
SirXunian <397939361@qq.com>
2016-01-08 13:43:27 +0800

    light bug 修改

--- shader/diffuse_fragmentEnd.glsl
+++ shader/diffuse_fragmentEnd.glsl
@@ -1,7 +1,7 @@
 void main() {
 
-	float lambertTerm = min(1.0,max(0.0,dot(normal,normalize(eyedir)))) * 0.5 + 0.5;		  
-	diffuse.xyz = diffuse.xyz * lambertTerm  ;
+	//float lambertTerm = min(1.0,max(0.0,dot(normal,normalize(eyedir)))) * 0.5 + 0.5;		  
+	//diffuse.xyz = diffuse.xyz * lambertTerm  ;
 
 	ttt.xyz = materialSource.ambient.xyz * materialSource.ambientPower;
 	light.xyz = light.xyz + ttt.xyz;
