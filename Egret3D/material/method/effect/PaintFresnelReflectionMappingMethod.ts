9f5a714f422fdc665978b9ff7c23439f09c3bc86
SirXunian <397939361@qq.com>
2016-01-15 17:01:13 +0800

    fix Wireframe bug

--- Egret3D/material/method/effect/PaintFresnelReflectionMappingMethod.ts
+++ Egret3D/material/method/effect/PaintFresnelReflectionMappingMethod.ts
@@ -18,7 +18,6 @@
         private _maskColorR: number = 1.0;
         private _maskColorG: number = 0.0;
         private _maskColorB: number = 0.0;
-        private reflectValue: number = 1.0;
         /**
         * @language zh_CN
         * 创建一个新的 EnvironmentMappingMethod 对象。
@@ -53,28 +52,6 @@
         */
         public get rimPower(): number {
             return this._rimPower;
-        }
-
-        /**
-        * @language zh_CN
-        * 设置反射值。
-        * @param value{Number}
-        * @version Egret 3.0
-        * @platform Web,Native
-        */
-        public set reflect(value: number) {
-            this.reflectValue = value;
-        }
-
-        /**
-        * @language zh_CN
-        * 获取反射值。
-        * @returns {Number}
-        * @version Egret 3.0
-        * @platform Web,Native
-        */
-        public get reflect(): number {
-            return this.reflectValue;
         }
 
         /**
@@ -183,7 +160,6 @@
          */
         public activateEffect(context3D: Context3D, usage: MethodUsageData, materialData: MaterialData, modeltransform: Matrix4_4, camera3D: Camera3D, geometry: GeometryBase, animation: IAnimation) {
             this.context3D = context3D;
-            usage["reflectValue"] = context3D.getUniformLocation(usage.program3D, "reflectValue");
             usage["maskColor"] = context3D.getUniformLocation(usage.program3D, "maskColor"); 
             usage["rimPower"] = context3D.getUniformLocation(usage.program3D, "rimPower");
             usage["envLightPower"] = context3D.getUniformLocation(usage.program3D, "envLightPower");
@@ -204,7 +180,6 @@
          * @platform Web,Native
          */
         public updataEffect(context3D: Context3D, usage: MethodUsageData, materialData: MaterialData, modeltransform: Matrix4_4, camera3D: Camera3D, geometry: GeometryBase, animation: IAnimation) {
-            context3D.gl.uniform1f(usage["reflectValue"], this.reflectValue);
             context3D.gl.uniform3f(usage["maskColor"], this._maskColorR, this._maskColorG, this._maskColorB);
             context3D.gl.uniform1f(usage["rimPower"], this._rimPower);
             context3D.gl.uniform1f(usage["envLightPower"], this._envLightPower);
